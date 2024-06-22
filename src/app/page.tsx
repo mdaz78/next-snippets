52bb7d5f781cb061861da1dc692ffc441e4802ec
Md Abu Zafar <mdabuzafar78@gmail.com>
2024-06-22 15:59:09 +0530

    feat: create page to view all the created snippets

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,31 @@
 import { getAllSnippetsFromDB } from '@/db';
+import Link from 'next/link';
 
 export default async function Home() {
   const snippets = await getAllSnippetsFromDB();
 
-  const renderedSnippets = snippets.map((snippet) => (
-    <div key={snippet.id}>
-      <h2>{snippet.title}</h2>
-      <code>
-        <pre>{snippet.code}</pre>
-      </code>
-    </div>
+  const renderedSnippets = snippets.map(({ id, title }) => (
+    <Link
+      href={`/snippets/${id}`}
+      className='flex justify-between items-center p-2 border rounded'
+      key={id}
+    >
+      {title}
+    </Link>
   ));
 
-  return <div>{renderedSnippets}</div>;
+  return (
+    <div className='m-5 space-y-5'>
+      <div className='flex justify-between items-center'>
+        <h1 className='text-xl font-bold'>Snippets</h1>
+        <Link
+          href={'/snippets/new'}
+          className='border rounded px-5 py-2 bg-slate-100'
+        >
+          New Snippet
+        </Link>
+      </div>
+      <div className='flex flex-col gap-2'>{renderedSnippets}</div>
+    </div>
+  );
 }
