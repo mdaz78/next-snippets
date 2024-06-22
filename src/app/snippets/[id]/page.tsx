37aa51322f9b0e20a1eb431a0c5f5633c52e891a
Md Abu Zafar <mdabuzafar78@gmail.com>
2024-06-22 15:59:19 +0530

    feat: create snippet details page

--- src/app/snippets/[id]/page.tsx
+++ src/app/snippets/[id]/page.tsx
@@ -1,5 +1,7 @@
 import { getASnippetByIdFromDB } from '@/db';
+import Link from 'next/link';
 import { notFound } from 'next/navigation';
+import { IoCaretBackOutline } from 'react-icons/io5';
 
 interface SnippetDetailsProps {
   params: {
@@ -16,7 +18,29 @@ const SnippetDetailsPage = async ({ params }: SnippetDetailsProps) => {
     return notFound();
   }
 
-  return <div>SnippetDetailsPage</div>;
+  return (
+    <div>
+      <div className='flex m-4 justify-between items-center'>
+        <div className='flex items-center space-x-4'>
+          <Link href='/'>
+            <IoCaretBackOutline />
+          </Link>
+          <h1 className='text-xl font-bold'>{snippet.title}</h1>
+        </div>
+        <div className='space-x-2'>
+          <button className='px-5 py-1 text-sm border rounded' type='button'>
+            Edit
+          </button>
+          <button className='px-5 py-1 text-sm border rounded' type='button'>
+            Delete
+          </button>
+        </div>
+      </div>
+      <pre className='p-3 border rounded bg-gray-200 border-gray-200'>
+        <code>{snippet.code}</code>
+      </pre>
+    </div>
+  );
 };
 
 export default SnippetDetailsPage;
