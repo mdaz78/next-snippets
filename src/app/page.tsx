5b854a8de2969369da3eea6efa84919fb79fdd0a
Md Abu Zafar <mdabuzafar78@gmail.com>
2024-06-22 13:45:58 +0530

    feat: Update Home page to render snippets from the database

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,16 @@
-export default function Home() {
-  return <div>Home Page</div>;
+import { getAllSnippetsFromDB } from '@/db';
+
+export default async function Home() {
+  const snippets = await getAllSnippetsFromDB();
+
+  const renderedSnippets = snippets.map((snippet) => (
+    <div key={snippet.id}>
+      <h2>{snippet.title}</h2>
+      <code>
+        <pre>{snippet.code}</pre>
+      </code>
+    </div>
+  ));
+
+  return <div>{renderedSnippets}</div>;
 }
