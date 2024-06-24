0f04cfa38869841568d45a704cf604619826bb5f
Md Abu Zafar <mdabuzafar78@gmail.com>
2024-06-24 08:49:47 +0530

    style: minor ui improvements

--- src/app/snippets/new/page.tsx
+++ src/app/snippets/new/page.tsx
@@ -1,12 +1,20 @@
+'use client';
 import { createSnippet } from '@/actions/snippets';
+import Link from 'next/link';
 import { useFormState } from 'react-dom';
+import { IoCaretBackOutline } from 'react-icons/io5';
 
 const SnippetCreatePage = () => {
   const [formState, formAction] = useFormState(createSnippet, { message: '' });
 
   return (
     <form className='' action={formAction}>
-      <h3 className='font-bold m-3'>Create a snippet</h3>
+      <div className='flex items-center space-x-2 my-3'>
+        <Link href='/'>
+          <IoCaretBackOutline />
+        </Link>
+        <h3 className='font-bold'>Create a snippet</h3>
+      </div>
       <div className='flex flex-col gap-4'>
         <div className='flex gap-4'>
           <label className='w-12' htmlFor='title'>
@@ -35,9 +43,14 @@ const SnippetCreatePage = () => {
           <div className='text-red-500'>{formState.message}</div>
         )}
 
-        <button type='submit' className='rounded p-2 bg-blue-200'>
-          Create
-        </button>
+        <div className='flex justify-end'>
+          <button
+            type='submit'
+            className='rounded py-2 px-8  bg-black text-white flex space-x-2 items-center'
+          >
+            <span className='text-sm'>Create</span>
+          </button>
+        </div>
       </div>
     </form>
   );
